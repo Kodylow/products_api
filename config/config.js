2d382994de756b51964ed527f1ce3cb22d624e7a
Kody Low <kodylow7@gmail.com>
2021-10-08 15:19:35 -0700

    fixed

--- config/config.js
+++
@@ -1,3 +0,0 @@
-var API_KEY = "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";
-
-module.exports = { API_KEY };
--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-const express = require('express');
-const app = express();
+const app = require("./app");
 
-app.listen(3000, )
-
-
-const { Client } = require('pg');
-const fs = require('fs');
-var sql = fs.readFileSync('./database/schema.sql').toString();
-
-const client = new Client({
-  user: 'postgres',
-  password: 'Reximus1234Prime!@#$',
-  host: 'localhost',
-  port: 5432,
-  database: 'atelier_db'
-});
-
-client.connect()
-.then(() => console.log("Connected to postgres db"))
-.then(() => client.query(sql, (err, res) => {
-  if(err){
-    console.log(err);
-    process.exit(1);
-  }
-  process.exit(0);
-  })
-)
-.catch(() => console.log("Error"))
-.finally(() => client.end());
+app.listen(4001, () => console.log("Server running on port 4001"));
